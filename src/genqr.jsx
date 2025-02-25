a963c59320998139e4b1a401c13a3fd86e109172
Ravula Akshith <ravulaakshith1@gmail.com>
2025-02-25 20:22:51 +0530

    Fix

--- src/genqr.jsx
+++ src/genqr.jsx
@@ -18,7 +18,7 @@ const QRCodeGenerator = () => {
         }
 
         try {
-            const response = await axios.post("https://bicycle-stand-locker-backend-phm9.onrender.com/generate", { locationid: locationId });
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/generate`, { locationid: locationId });
 
             if (response.status === 200 || response.status === 201) {
                 setMessage("Successfully Generated. Thank you!");
