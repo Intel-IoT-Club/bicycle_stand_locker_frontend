5434b02c60e2061de638bade704fba688bfe79a3
NAREN SUNDAR L <66767561+NarenSundar007@users.noreply.github.com>
2025-04-28 19:25:49 +0530

    removed

--- README.md
+++
@@ -1,3 +0,0 @@
-# Quick Tips for checking
-### 1) ```npm run build ```
-### 2) ```npm run start```
--- config-overrides.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = function (config) {
-    config.output.path = path.join(__dirname, 'dist');  // Output to `dist`
-    return config;
-};
