e52194731cc9b72e59ff2919d2990bb53160641b
SriCharan <charan3407@gmail.com>
2025-09-17 22:34:01 +0530

    Added About us page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import Header from "./components/Auth/header";
 import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
           <Route path="/" element={<AuthPage />} />
           <Route path="/login" element={<AuthPage />} />
           <Route path="/signup" element={<AuthPage />} />
+          {/* <Route path="/about" element={<AboutPage />} /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
         </Routes>
       </Router>
     </>
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,72 @@
+import Header from "../components/Auth/header";
+import UnlockIcon from "../assets/Unlock_Icon.png";
+import LocationIcon from "../assets/Location_Icon.png";
+import CardWallet from "../assets/Card_Wallet_Icon.png";
+import BarChart from "../assets/Bar_Chart_Icon.png";
+
+const AboutPage = () => {
+  return (
+    <>
+    <Header/>
+    <div className="bg-[#F9F8E9] font-afacad h-auto m-0">
+      {/* Header */}
+      <div className="bg-gradient-to-b from-[#024A48] via-[#082323] to-[#101010] text-center py-6">
+        <h2 className="text-5xl font-bold text-white">About Us</h2>
+      </div>
+
+      {/* Content */}
+      <div className="flex-1 w-full px-24 py-24 grid grid-cols-1 lg:grid-cols-2 items-center">
+        {/* Left Section */}
+        <div className="flex flex-col justify-center items-center text-center gap-y-8 px-24 py-12">
+          <h3 className="font-bold text-6xl">
+            Connecting Our Campus, Faster
+          </h3>
+          <p className="font-normal text-3xl">
+            We are a team of innovators from the IoT Club dedicated to building a
+            smart, sustainable future. Our Bicycle Rental Project leverages IoT and
+            cloud technology to make commuting convenient, secure, and eco-friendly
+            by reducing manual effort and providing a seamless user experience.
+          </p>
+        </div>
+
+        {/* Right Section */}
+        <div className="grid grid-cols-2 gap-18 justify-center items-center px-24">
+          {/* Feature 1 */}
+          <div className="flex flex-col items-center text-center">
+            <div className="bg-[#016766] p-9 rounded-full hover:scale-105 transition">
+              <img src={UnlockIcon} className="h-18 w-18" />
+            </div>
+            <p className="font-semibold text-3xl">Tap & Ride Access</p>
+          </div>
+
+          {/* Feature 2 */}
+          <div className="flex flex-col items-center text-center">
+            <div className="bg-[#016766] p-9 rounded-full hover:scale-105 transition">
+              <img src={LocationIcon} className="h-18 w-18" />
+            </div>
+            <p className="font-semibold text-3xl">Live Bike Map</p>
+          </div>
+
+          {/* Feature 3 */}
+          <div className="flex flex-col items-center text-center">
+            <div className="bg-[#016766] p-9 rounded-full hover:scale-105 transition">
+              <img src={CardWallet} className="h-18 w-18" />
+            </div>
+            <p className="font-semibold text-3xl">Effortless Payments</p>
+          </div>
+
+          {/* Feature 4 */}
+          <div className="flex flex-col items-center text-center">
+            <div className="bg-[#016766] p-9 rounded-full hover:scale-105 transition">
+              <img src={BarChart} className="h-18 w-18" />
+            </div>
+            <p className="font-semibold text-3xl">Ride Dashboard</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default AboutPage;
